419ed24ffb31b96a4b0d92c1b85f54b0edf9ba80
Scott Arciszewski <scott@resonantcore.net>
2014-12-27 02:40:05 -0500

    Add some filters to the view.

--- src/View.php
+++ src/View.php
@@ -20,6 +20,34 @@ class View
             $this->default_file = $default_template;
         }
         $this->engine = $engine;
+        
+        // Now let's add our custom filters!
+        $this->filter('addslashes', 'addslashes');
+        $this->filter('preg_quote', 'preg_quote');
+        
+        // Add a |cachebust filter; uses HMAC-SHA1 of a file's contents and its
+        // modification time so as to break browser caches
+        $this->filter('cachebust', function ($relative_path) {
+            if ($relative_path[0] !== '/') {
+                $relative_path = '/' . $relative_path;
+            }
+            $absolute = $_SERVER['DOCUMENT_ROOT'] . $relative_path;
+            if (\is_readable($absolute)) {
+                // It was found. let's cachebust it!
+                return $relative_path.'?'.\base64_encode(hash_hmac(
+                    'sha1',
+                    \file_get_contents($absolute),
+                    \filemtime($absolute),
+                    true
+                ));
+            }
+            // Not found? Let's add a special tag for it too:
+            return $relative_path.'?404NotFound';
+        });
+        $this->engine->addGlobal('_GET', $_GET);
+        $this->engine->addGlobal('_POST', $_POST);
+        $this->engine->addGlobal('_SESSION', isset($_SESSION) ? $_SESSION : []);
+        $this->engine->addGlobal('_COOKIE', $_COOKIE);
     }
 
     /**
@@ -38,6 +66,19 @@ class View
         return $this->template->render(
             empty($template) ? $this->default_file : $template,
             $params
+        );
+    }
+    
+    /**
+     * Add a filter to Twig
+     * 
+     * @param string $name - Name to access n Twig
+     * @param callable $func - function to apply
+     */
+    public function filter($name, callable $func)
+    {
+        return $this->engine->addFilter(
+            new \Twig_SimpleFilter($name, $func)
         );
     }
 }
